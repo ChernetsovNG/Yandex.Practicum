264a78d6796837bdc595bdedaf9f75ce3f835f0c
Nikita Chernetsov <nchernetsov@yandex-team.ru>
2021-09-13 13:21:15 +0300

    sprint 03: решены 10 задач контеста

---
+++ Algorithms/sprint_03/contest/D.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	const maxCapacity = 32 * 10_000
+	buffer := make([]byte, maxCapacity)
+	scanner.Buffer(buffer, maxCapacity)
+	scanner.Split(bufio.ScanLines)
+
+	var line string
+
+	// читаем количество детей
+	var n int
+
+	scanner.Scan()
+	line = scanner.Text()
+	n, _ = strconv.Atoi(line)
+
+	// читаем факторы жадности детей
+	greedyFactors := make([]int, n)
+
+	var value int
+	scanner.Scan()
+	row := scanner.Text()
+	values := strings.Split(row, " ")
+	for i := 0; i < n; i++ {
+		value, _ = strconv.Atoi(values[i])
+		greedyFactors[i] = value
+	}
+
+	// читаем количество печенек
+	var m int
+
+	scanner.Scan()
+	line = scanner.Text()
+	m, _ = strconv.Atoi(line)
+
+	// читаем размеры печенек
+	sizes := make([]int, m)
+
+	scanner.Scan()
+	row = scanner.Text()
+	values = strings.Split(row, " ")
+	for i := 0; i < m; i++ {
+		value, _ = strconv.Atoi(values[i])
+		sizes[i] = value
+	}
+
+	// считаем, сколько детей останутся довольными
+	sort.Ints(greedyFactors)
+	sort.Ints(sizes)
+
+	i, j := 0, 0
+	var greedy, size int
+	result := 0
+	for true {
+		if i >= n || j >= m {
+			break
+		}
+
+		greedy = greedyFactors[i]
+		size = sizes[j]
+
+		if size >= greedy { // ребёнок может взять эту печеньку
+			result += 1
+
+			i += 1
+			j += 1
+		} else { // иначе рассматриваем следующую печеньку
+			j += 1
+		}
+	}
+
+	fmt.Println(result)
+}
---
+++ Algorithms/sprint_03/contest/G.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func countingSort(array []int, k int) {
+	countedValues := make([]int, k+1)
+	for _, value := range array {
+		countedValues[value] += 1
+	}
+	index := 0
+	for value := 0; value <= k; value++ {
+		for amount := 1; amount <= countedValues[value]; amount++ {
+			array[index] = value
+			index += 1
+		}
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	const maxCapacity = 32 * 1_000_000
+	buffer := make([]byte, maxCapacity)
+	scanner.Buffer(buffer, maxCapacity)
+	scanner.Split(bufio.ScanLines)
+
+	var line string
+
+	// читаем количество предметов в гардеробе
+	var n int
+
+	scanner.Scan()
+	line = scanner.Text()
+	n, _ = strconv.Atoi(line)
+
+	// читаем массив цветов предметов
+	colors := make([]int, n)
+
+	var value int
+	scanner.Scan()
+	row := scanner.Text()
+	values := strings.Split(row, " ")
+	for i := 0; i < n; i++ {
+		value, _ = strconv.Atoi(values[i])
+		colors[i] = value
+	}
+
+	countingSort(colors, 2)
+
+	var b bytes.Buffer
+	var color int
+	for i := 0; i < n-1; i++ {
+		color = colors[i]
+		fmt.Fprintf(&b, "%d ", color)
+	}
+	if n > 0 {
+		fmt.Fprintf(&b, "%d", colors[n-1])
+	}
+
+	fmt.Print(b.String())
+}
