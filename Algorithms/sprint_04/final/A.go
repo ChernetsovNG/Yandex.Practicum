5bb55cc7a549e0432dde07d903f6bd58a4c77b1a
ChernetsovNG <n.chernetsov86@gmail.com>
2021-10-02 19:14:40 +0300

    sprint 04: final - A => удалось победить TL путём введения кеша

--- Algorithms/sprint_04/final/A.go
+++ Algorithms/sprint_04/final/A.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
 )
 
+/*
+https://contest.yandex.ru/contest/24414/run-report/53763938/
+
+-- ПРИНЦИП РАБОТЫ --
+Каждый документ мы разбиваем на слова, и для каждого слова сохраняем словарь, в котором ключом является
+номер документа, а значением - количество раз, сколько данное слово встречается в документе с этим номером.
+
+Далее, когда выполняется поисковый запрос, то мы выделяем в нём уникальные слова, и для каждого слова
+находим, в каких документах и сколько раз оно встречалось. Далее выполняется суммирование по всем
+словам запроса, и таким образом вычисляется релевантность всех документов. Полученный результат сортируется
+по релевантности, и берутся первые 5 элементов массива.
+
+Для оптимизации решения результат обработки каждого поискового запроса кешируется.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Решение соответствует условию задания в части определения релевантности документа
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Пусть n - количество документов, а w1 - среднее количество слов в каждом документе. Пусть каждое слово из
+документов в среднем содержится в f1 документах.
+Тогда построение поискового индекса выполняется за время O(n * w).
+
+Пусть m - количество запросов, а w2 - среднее количество слов в каждом запросе. Пусть каждое слово поискового
+запроса в среднем содержится в f2 документах из индекса.
+Тогда получение топ-5 документов по релевантности выполняется за время O(m * (w2 + w2*f2 + f2*log(f2)))
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+Мы сохраняем поисковый индекс размера O(w1 * f1). Дополнительно сохраняется кеш поисковых запросов, размер
+которого зависит от количества повторяющихся запросов
+*/
 func main() {
 	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
-	const maxCapacity = 32 * 1000
-	buffer := make([]byte, maxCapacity)
-	scanner.Buffer(buffer, maxCapacity)
 	scanner.Split(bufio.ScanLines)
+	writer := bufio.NewWriter(os.Stdout)
 
 	var line string
 
@@ -54,10 +81,20 @@ func main() {
 	m, _ = strconv.Atoi(line)
 
 	// читаем и обрабатываем запросы
+	searchCache := newSearchCache()
+
 	for i := 0; i < m; i++ {
 		// 1. считываем запрос
 		scanner.Scan()
 		line = scanner.Text()
+
+		// если результат был закеширован, то используем его
+		cachedResult, ok := searchCache.cacheElements[line]
+		if ok {
+			topFiveDocuments := cachedResult.topFiveDocuments
+			printArray(writer, topFiveDocuments)
+			continue
+		}
 
 		// 2. по индексу считаем релевантность всех документов
 		uniqueWords := make(map[string]bool) // в запросе нужно рассматривать только уникальные слова
@@ -66,6 +103,7 @@ func main() {
 			uniqueWords[words[j]] = true
 		}
 
+		// для каждого документа - его релевантность
 		documentsRelevance := make(map[int]int)
 		for word := range uniqueWords {
 			documentsCounts, ok := searchIndex[word]
@@ -78,10 +116,10 @@ func main() {
 
 		// 3. сортируем документы по релевантности
 		relevanceArray := make([]kv, len(documentsRelevance))
-		i := 0
+		arrIndex := 0
 		for documentIndex, relevance := range documentsRelevance {
-			relevanceArray[i] = kv{documentIndex + 1, relevance}
-			i++
+			relevanceArray[arrIndex] = kv{documentIndex + 1, relevance}
+			arrIndex++
 		}
 
 		sort.Slice(relevanceArray, func(i, j int) bool {
@@ -97,8 +135,18 @@ func main() {
 		})
 
 		// выводим лучшие 5 документов
-		printArray(relevanceArray)
+		printArrayOfPairs(writer, relevanceArray)
+
+		// кешируем результат
+		resultForCaching := make([]int, len(relevanceArray))
+		for k := 0; k < len(relevanceArray); k++ {
+			pair := relevanceArray[k]
+			resultForCaching[k] = pair.Key
+		}
+		searchCache.cacheElements[line] = newCacheElement(resultForCaching)
 	}
+
+	writer.Flush()
 }
 
 type kv struct {
@@ -106,7 +154,24 @@ type kv struct {
 	Value int
 }
 
-func printArray(array []kv) {
+// Для ускорения поиска кешируем результаты: для каждого запроса сохраняем топ-5 документов
+type SearchCache struct {
+	cacheElements map[string]CacheElement
+}
+
+func newSearchCache() SearchCache {
+	return SearchCache{make(map[string]CacheElement)}
+}
+
+type CacheElement struct {
+	topFiveDocuments []int
+}
+
+func newCacheElement(topFiveDocuments []int) CacheElement {
+	return CacheElement{topFiveDocuments}
+}
+
+func printArray(writer *bufio.Writer, array []int) {
 	if len(array) == 0 {
 		return
 	}
@@ -117,8 +182,27 @@ func printArray(array []kv) {
 		k = len(array)
 	}
 	for i := 0; i < k-1; i++ {
-		fmt.Printf("%d", array[i].Key)
-		fmt.Print(" ")
+		writer.WriteString(strconv.Itoa(array[i]))
+		writer.WriteString(" ")
 	}
-	fmt.Printf("%d\n", array[k-1].Key)
+	writer.WriteString(strconv.Itoa(array[k-1]))
+	writer.WriteString("\n")
+}
+
+func printArrayOfPairs(writer *bufio.Writer, array []kv) {
+	if len(array) == 0 {
+		return
+	}
+	var k int
+	if len(array) >= 5 {
+		k = 5
+	} else {
+		k = len(array)
+	}
+	for i := 0; i < k-1; i++ {
+		writer.WriteString(strconv.Itoa(array[i].Key))
+		writer.WriteString(" ")
+	}
+	writer.WriteString(strconv.Itoa(array[k-1].Key))
+	writer.WriteString("\n")
 }
