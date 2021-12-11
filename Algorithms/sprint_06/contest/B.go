c58e9c5755500ecc79a9b750440538fbfa0612ff
ChernetsovNG <n.chernetsov86@gmail.com>
2021-12-11 12:37:37 +0300

    sprint 06: contest - задача B "Перевести список ребер в матрицу смежности" - добавлено решение

---
+++ Algorithms/sprint_06/contest/B.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	scanner.Split(bufio.ScanLines)
+
+	var line string
+
+	// читаем число вершин и рёбер
+	var n, m int
+
+	scanner.Scan()
+	line = scanner.Text()
+
+	nm := strings.Split(line, " ")
+	n, _ = strconv.Atoi(nm[0])
+	m, _ = strconv.Atoi(nm[1])
+
+	// матрица смежности
+	adjacencyMatrix := make([][]int, n)
+	for i := range adjacencyMatrix {
+		adjacencyMatrix[i] = make([]int, n)
+	}
+
+	// читаем информацию о рёбрах (в виде вершин, соединяемых ребром)
+	for i := 0; i < m; i++ {
+		scanner.Scan()
+		line = scanner.Text()
+		uv := strings.Split(line, " ")
+		u, _ := strconv.Atoi(uv[0])
+		v, _ := strconv.Atoi(uv[1])
+
+		adjacencyMatrix[u-1][v-1] = 1
+	}
+
+	// выводим матрицу смежности
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-1; j++ {
+			fmt.Print(strconv.Itoa(adjacencyMatrix[i][j]) + " ")
+		}
+		fmt.Print(strconv.Itoa(adjacencyMatrix[i][n-1]))
+		if i < n-1 {
+			fmt.Print("\n")
+		}
+	}
+}
