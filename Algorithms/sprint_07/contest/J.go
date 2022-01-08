a11315f4869a01bb178d5bcc8bd08d0d5ba8302f
ChernetsovNG <n.chernetsov86@gmail.com>
2022-01-08 13:09:29 +0300

    sprint 07: contest - задача J "Путешествие" - добавлена заготовка с чтением входных данных

---
+++ Algorithms/sprint_07/contest/J.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+
+	var line string
+
+	// читаем число различных туристических мест
+	var n int
+
+	scanner.Scan()
+	line = scanner.Text()
+
+	n, _ = strconv.Atoi(line)
+
+	// читаем рейтинги достопримечательностей
+	r := make([]int, n)
+
+	scanner.Scan()
+	line = scanner.Text()
+	values := strings.Split(line, " ")
+
+	var value int
+	for i := 0; i < n; i++ {
+		value, _ = strconv.Atoi(values[i])
+		r[i] = value
+	}
+}
