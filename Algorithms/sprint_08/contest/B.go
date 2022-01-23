ddff6716611dfdce4909a907c59adb9cc18b5813
ChernetsovNG <n.chernetsov86@gmail.com>
2022-01-23 17:38:19 +0300

    sprint 08: contest - задача B "Пограничный контроль" - не придумал, как решить

---
+++ Algorithms/sprint_08/contest/B.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	const maxCapacity = 8 * 100000
+	buffer := make([]byte, maxCapacity)
+	scanner.Buffer(buffer, maxCapacity)
+	scanner.Split(bufio.ScanLines)
+
+	// читаем имя из паспорта
+	scanner.Scan()
+	passportName := scanner.Text()
+
+	// читаем имя из базы данных
+	scanner.Scan()
+	databaseName := scanner.Text()
+
+	// сравниваем строки
+	// допустима ситуация, когда имя человека в базе отличается от имени в паспорте
+	// на одну замену, одно удаление или одну вставку символа
+	passportSymbols := []rune(passportName)
+	databaseSymbols := []rune(databaseName)
+
+	deltasCount := 0
+
+	for i := 0; i < len(passportSymbols); i++ {
+		passportSymbol := passportSymbols[i]
+		databaseSymbol := databaseSymbols[i]
+
+		if passportSymbol != databaseSymbol {
+		}
+	}
+
+	fmt.Print("OK")
+	fmt.Print("FAIL")
+}
