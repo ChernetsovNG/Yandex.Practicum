7372961b20bed4f2cf7098cd2145285c88b95d98
Nikita Chernetsov <nchernetsov@yandex-team.ru>
2022-01-31 19:54:25 +0300

    sprint 08: final - задача B "Шпаргалка" - пробуем подход с префиксным деревом => не получается

--- Algorithms/sprint_08/final/B.go
+++ Algorithms/sprint_08/final/B.go
@@ -7,6 +7,20 @@ import (
 	"strconv"
 )
 
+type Node struct {
+	isTerminal bool
+	// массив переходов:
+	// на позиции с индексом i хранится ссылка на ребро, отвечающее i-му символу алфавита
+	edges []*Node
+	// в терминальных узлах будем хранить слова, которые их образуют
+	s string
+}
+
+func newNode() *Node {
+	edges := make([]*Node, 26)
+	return &Node{false, edges, ""}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	const maxCapacity = 8 * 100000
@@ -27,8 +41,8 @@ func main() {
 	line = scanner.Text()
 	n, _ = strconv.Atoi(line)
 
-	// читаем допустимые к использованию слова и добавляем их в множество (для быстрой проверки вхождения)
-	words := makeSet()
+	// читаем допустимые к использованию слова и добавляем их в префиксное дерево
+	prefixTree := newNode()
 
 	// запоминаем длину самого длинного слова из словаря
 	maxLen := -1
@@ -36,7 +50,7 @@ func main() {
 	for i := 0; i < n; i++ {
 		scanner.Scan()
 		line = scanner.Text()
-		words.add(line)
+		addString(prefixTree, line)
 		if len(line) > maxLen {
 			maxLen = len(line)
 		}
@@ -56,16 +70,45 @@ func main() {
 	// или если для любого j от 1 до i-1 dp[j] = true и символы [j+1 ... i] являются словом из словаря
 	textSymbols := []rune(text)
 
-	for i := 1; i <= len(text); i++ {
-		if words.contains(string(textSymbols[0:i])) {
-			dp[i] = true
-			continue
-		}
-		for j := i - maxLen; j <= i-1; j++ {
-			if j >= 1 && dp[j] == true && words.contains(string(textSymbols[j:i])) {
-				dp[i] = true
-				break
+	currentNode := prefixTree
+	treeDepth := 0
+
+	for i := 0; i < len(text); i++ {
+		// сдвигаем курсор на символ
+		pos := position(textSymbols[i])
+
+		// спускаемся в дереве
+		currentNode = currentNode.edges[pos]
+		treeDepth += 1
+
+		if currentNode == nil { // дальше не можем опускаться в дереве => начинаем спуск от корня
+			currentNode = prefixTree
+			treeDepth = 0
+
+			dp[i+1] = false
+		} else if currentNode.isTerminal && i+1-treeDepth >= 0 && dp[i+1-treeDepth] == true {
+			dp[i+1] = true
+
+			// проверяем следующий символ, надо ли сбросить дерево снова до корня?
+			i++
+			if i < len(text) {
+				pos = position(textSymbols[i])
+
+				prevNode := currentNode
+				currentNode = currentNode.edges[pos]
+				treeDepth += 1
+
+				if currentNode == nil {
+					currentNode = prefixTree
+					treeDepth = 0
+				} else {
+					currentNode = prevNode
+					treeDepth -= 1
+				}
+				i--
 			}
+		} else {
+			dp[i+1] = false
 		}
 	}
 
@@ -76,22 +119,27 @@ func main() {
 	}
 }
 
-// множество строк
-func makeSet() *Set {
-	return &Set{
-		container: make(map[string]bool),
+// функция построения префиксного дерева - добавления нового символа в дерево
+func addString(root *Node, s string) {
+	currentNode := root
+	for _, symbol := range s {
+		pos := position(symbol)
+		// если из currentNode нет перехода по символу:
+		if currentNode.edges[pos] == nil {
+			// создать узел newNode
+			// создать ребро symbol из currentNode в newNode
+			currentNode.edges[pos] = newNode()
+		}
+		// сдвинуться на следующий символ
+		currentNode = currentNode.edges[pos]
 	}
+
+	// пометить currentNode как терминальный символ
+	currentNode.isTerminal = true
+	currentNode.s = s
 }
 
-type Set struct {
-	container map[string]bool
-}
-
-func (c *Set) contains(key string) bool {
-	_, exists := c.container[key]
-	return exists
-}
-
-func (c *Set) add(key string) {
-	c.container[key] = true
+// ord(a) = 97
+func position(r rune) int {
+	return int(r) - 97
 }
